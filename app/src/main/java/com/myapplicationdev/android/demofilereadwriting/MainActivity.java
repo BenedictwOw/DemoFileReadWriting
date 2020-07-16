ab461d17269d6dfb45905f9492dd1888cda710c8
18044928 <18044928@myrp.edu.sg>
2020-07-16 11:33:46 +0800

    final

--- app/src/main/java/com/myapplicationdev/android/demofilereadwriting/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demofilereadwriting/MainActivity.java
@@ -9,23 +9,38 @@ import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnWrite,btnRead;
+    TextView tvContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnWrite = findViewById(R.id.btnWrite);
+        btnRead = findViewById(R.id.btnRead);
+        tvContent = findViewById(R.id.tv);
         int permissionCheck_Storage = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         if(permissionCheck_Storage != PermissionChecker.PERMISSION_GRANTED){
             Toast.makeText(this,"Permission not granted",Toast.LENGTH_SHORT).show();
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},0 );
             finish();
         }
-        String folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
+        final String folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyFolder";
         File folder = new File(folderLocation);
         if(folder.exists() == false){
             boolean result = folder.mkdir();
@@ -33,5 +48,48 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("File Read/Write","Folder created");
             }
         }
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File targetFile = new File(folderLocation, "data.txt");
+                try{
+                    FileWriter writer = new FileWriter(targetFile,true);
+                    writer.write("Hello World"+"\n");
+                    writer.flush();
+                    writer.close();
+                } catch (IOException e) {
+                    Toast.makeText(MainActivity.this,"Failed to write!",Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File targetFile = new File(folderLocation, "data.txt");
+                if(targetFile.exists() == true){
+                    String data = "";
+                    try{
+                        FileReader reader = new FileReader(targetFile);
+                        BufferedReader br = new BufferedReader(reader);
+                        String line = br.readLine();
+                        while (line != null){
+                            data += line +"\n";
+                            line = br.readLine();
+                            while(line != null){
+                                data += line + "\n";
+                                line = br.readLine();
+                            }
+                        }
+                        br.close();
+                        reader.close();
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this,"Failed to read!",Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                    tvContent.setText(data);
+                }
+            }
+        });
     }
 }
